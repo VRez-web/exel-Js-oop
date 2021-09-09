8d075be0ea95a17da0bebe03430a06461657128f
Victor Reznichenko <vit.79@bk.ru>
2021-09-09 10:25:24 +0300

    feat: add checked cell

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,8 +3,19 @@ const CODES = {
   Z: 90,
 };
 
-function toCell(_, col) {
-  return `<div class="cell" contenteditable data-col="${col}"></div>`;
+function toCell(row) {
+  return function(_, col) {
+    return `
+         <div 
+          class="cell"
+          contenteditable 
+          data-col="${col}"
+          data-type="cell"
+          data-id="${row}:${col}"
+          >
+          </div>
+          `;
+  };
 }
 
 function toColumn(col, index) {
@@ -41,9 +52,12 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols));
 
-  for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount).fill('').map(toCell).join('');
-    rows.push(createRow(i + 1, cells));
+  for (let row = 0; row < rowsCount; row++) {
+    const cells = new Array(colsCount)
+      .fill('')
+      .map(toCell(row))
+      .join('');
+    rows.push(createRow(row + 1, cells));
   }
   return rows.join('');
 }
