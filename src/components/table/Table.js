37e00163c4769fc9e34256398939197a85f3bf87
Victor Reznichenko <vit.79@bk.ru>
2022-01-14 12:31:17 +0300

    feat: realize state for columns, rows and cells

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,10 @@
 import {ExcelComponent} from '../../core/ExcelComponent';
 import {createTable} from './table.template';
 import {resizeHandler} from './table.resize';
-import {isCell, matrix, shouldResize, nextSelector} from './table.functions';
+import {isCell, matrix, nextSelector, shouldResize} from './table.functions';
 import {TableSelection} from './TableSelection';
 import {$} from '../../core/dom';
-import {range} from "../../core/utils";
+import * as actions from '../../store/actions'
 
 export class Table extends ExcelComponent {
     static className = 'excel__table';
@@ -18,7 +18,7 @@ export class Table extends ExcelComponent {
     }
 
     toHTML() {
-        return createTable(20);
+        return createTable(20, this.store.getState());
     }
 
     prepare() {
@@ -38,9 +38,14 @@ export class Table extends ExcelComponent {
         this.$emit('table:select', $cell)
     }
 
+    async resizeTable(event) {
+        const data = await resizeHandler(this.$root, event);
+        this.$dispatch(actions.tableResize(data))
+    }
+
     onMousedown(event) {
         if (shouldResize(event)) {
-            resizeHandler(this.$root, event);
+            this.resizeTable(event)
         } else if (isCell(event)) {
             const $target = $(event.target);
 
@@ -49,7 +54,7 @@ export class Table extends ExcelComponent {
                     .map(id => this.$root.find(`[data-id="${id}"]`))
                 this.selection.selectGroup($cells)
             } else {
-                this.selection.select($target);
+                this.selectCell($target);
             }
         }
     }
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,3 +1,14 @@
+import {TABLE_RESIZE} from "./types";
+
 export function rootReducer(state, action) {
-    return state
+    let field
+    switch (action.type) {
+        case TABLE_RESIZE:
+            field = action.data.type === 'col' ? 'colState' : 'rowState'
+            const prevState = state[field] || {}
+            prevState[action.data.id] = action.data.value
+            return {...state, [field]: prevState}
+        default:
+            return state
+    }
 }
